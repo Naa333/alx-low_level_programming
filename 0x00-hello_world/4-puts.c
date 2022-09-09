106d0dca752445daeceb5b4fde7a118984276412
Naa333 <naaamorkor33@gmail.com>
2022-09-08 17:38:26 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char *y = "Programming is like building a multilingual puzzle";
+{
+	char *y = "Programming is like building a multilingual puzzle;
 	puts(y);
 	return (0);
 }	
