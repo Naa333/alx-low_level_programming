9df04bc77e8c51865ee9f1dcd0fc0986ece00165
Naa333 <naaamorkor33@gmail.com>
2022-09-08 17:03:34 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+include <stdio.h>
+
+int main()
+{	
+	char *y = "Programming is like building a multilingual puzzle";
+	puts(y);
+	return 0;
+}	
