e6a4b5d867556c15753d3620d05ba5b498171746
Naa333 <naaamorkor33@gmail.com>
2022-09-09 18:19:11 -0700

    more use of variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - last digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char l[] = "Last digit of";
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	printf("%s %d is %d and is ", l, n, n % 10);
+	if (n % 10 > 5)
+	{
+	printf("greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+	printf("0\n");
+	}
+	else 
+	{
+	printf("less than 6 and not 0\n");
+	}
+	return (0);
+}
