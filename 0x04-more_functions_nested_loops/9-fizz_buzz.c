2a80ce10e31ac4a555160aba5d1c7d65903db040
Naa333 <naaamorkor33@gmail.com>
2022-09-15 19:49:04 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,25 +16,16 @@ int main(void)
 	for (i = 0; i <= 100; i++)
 	{
 		if (i == 100)
-		{
 			printf("%s", b);
-		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
 			printf("%s", c);
-		}
 		else if (i % 3 == 0)
-		{
 			printf("%s", a);
-		}
 		else if (i % 5 == 0)
-		{
 			printf("%s", b);
-		}
-		else 
-		{
+		else
 			printf("%d", i);
-		}
+		
 	}
 		printf("\n");
 		return (0);
