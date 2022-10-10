7a345737127383716e58c1bce235ffcf241a0af8
Naa333 <naaamorkor33@gmail.com>
2022-10-10 15:32:01 -0700

    a header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+/**
+ * file - main.h
+ */
+
+#include <stdlib.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
+#endif
+
