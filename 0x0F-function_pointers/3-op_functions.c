dbbfc55c2124e92fcc0a8eae5977ebfda2ef13d4
Naa333 <naaamorkor33@gmail.com>
2022-10-12 17:07:13 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of 2 integers
+ * @a: int 1
+ * @b: int 2
+ * 
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts 1 int from another
+ * @a: int 1
+ * @b: int 2
+ * 
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - mult 2 integers
+ * @a: int 1
+ * @b: int 2
+ * 
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - div 2 integers
+ * @a: int 1
+ * @b: int 2
+ * 
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - mod of 2 integers
+ * @a: int 1
+ * @b: int 2
+ * 
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
